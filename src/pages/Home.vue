a66730750f0d32707975c67a2f7cc079e36e3fb2
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-05-14 11:57:46 -0400

    adding content for mother's day 2023 poem, styling and particles background

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -4,256 +4,185 @@
 <script setup lang="ts">
 
     import { ref } from 'vue'
-    import { VueVentusSpinningMark } from '@obewds/vueventus'
-    import appVv from '../app.vv'
-    import VvAnchor from '../components/vv/anchors/VvAnchor.vue'
-    import VvButton from '../components/vv/buttons/VvButton.vue'
-    import VvCheckbox from '../components/vv/inputs/VvCheckbox.vue'
-    import VvEl from '../components/vv/elements/VvEl.vue'
-    import VvFa from '../components/vv/elements/VvFa.vue'
-    import VvFormGroup from '../components/vv/forms/VvFormGroup.vue'
-    import VvInput from '../components/vv/inputs/VvInput.vue'
-    import VvList from '../components/vv/lists/VvList.vue'
-    import VvListbox from '../components/vv/selects/VvListbox.vue'
-    import VvListItem from '../components/vv/lists/VvListItem.vue'
-    import VvPrism from '../components/vv/elements/VvPrism.vue'
-    import VvQuadFormGroup from '../components/vv/forms/VvQuadFormGroup.vue'
-    import VvRadio from '../components/vv/inputs/VvRadio.vue'
-    import VvRouterLink from '../components/vv/anchors/VvRouterLink.vue'
-    import VvSelect from '../components/vv/selects/VvSelect.vue'
-    import VvTextarea from '../components/vv/textareas/VvTextarea.vue'
+    // import VvFa from '../components/vv/elements/VvFa.vue'
+    // import { onMounted, ref } from 'vue'
+    // import VvEl from '../components/vv/elements/VvEl.vue'
 
-    const msg = 'Vue 3, Tailwind CSS, Headless UI, Vue Router, Vite & Typescript'
+    // const swap1 = ref(false)
 
-    const count = ref(0)
+    const heartsWorkspaceDiv = ref<null|HTMLDivElement>(null)
+
+    /*
+    onMounted( () => {
+
+        setInterval(function() {
+
+            var r_num = Math.floor(Math.random() * 40) + 1
+            var r_size = Math.floor(Math.random() * 65) + 10
+            var r_left = Math.floor(Math.random() * 100) + 1
+            var r_bg = Math.floor(Math.random() * 25) + 100
+            var r_time = Math.floor(Math.random() * 5) + 5
+
+            var heartDiv = document.createElement('div')
+
+            heartDiv.classList.add('heart')
+            heartDiv.setAttribute('width', r_size + 'px')
+            heartDiv.setAttribute('height', r_size + 'px')
+            heartDiv.setAttribute('left', r_left + '%')
+            heartDiv.setAttribute('background', 'rgba(255,' + (r_bg - 25) + ',' + r_bg + ',1)')
+            // heartDiv.setAttribute('-webkit-animation', 'love ' + r_time + 's ease')
+            // heartDiv.setAttribute('-moz-animation', 'love ' + r_time + 's ease')
+            // heartDiv.setAttribute('-ms-animation', 'love ' + r_time + 's ease')
+            heartDiv.setAttribute('animation', 'love ' + r_time + 's ease')
+
+            heartsWorkspaceDiv.value?.appendChild(heartDiv)
+
+            var heartDiv2 = document.createElement('div')
+
+            heartDiv2.classList.add('heart')
+            heartDiv2.setAttribute('width', (r_size - 10) + 'px')
+            heartDiv2.setAttribute('height', (r_size - 10) + 'px')
+            heartDiv2.setAttribute('left', (r_left + r_num) + '%')
+            heartDiv2.setAttribute('background', 'rgba(255,' + (r_bg - 25) + ',' + (r_bg + 25) + ',1)')
+            // heartDiv2.setAttribute('-webkit-animation', 'love ' + (r_time + 5) + 's ease')
+            // heartDiv2.setAttribute('-moz-animation', 'love ' + (r_time + 5) + 's ease')
+            // heartDiv2.setAttribute('-ms-animation', 'love ' + (r_time + 5) + 's ease')
+            heartDiv2.setAttribute('animation', 'love ' + (r_time + 5) + 's ease')
+
+            heartsWorkspaceDiv.value?.appendChild(heartDiv2)
+
+            // width:" + (r_size - 10) + "px;
+            // height:" + (r_size - 10) + "px;
+            // left:" + (r_left + r_num) + "%;
+            // background:rgba(255," + (r_bg - 25) + "," + (r_bg + 25) + ",1);
+            // -webkit-animation:love " + (r_time + 5) + "s ease;
+            // -moz-animation:love " + (r_time + 5) + "s ease;
+            // -ms-animation:love " + (r_time + 5) + "s ease;
+            // animation:love " + (r_time + 5) + "s ease
+
+            // $('.bg_heart').append("<div class='heart' style='width:" + r_size + "px;height:" + r_size + "px;left:" + r_left + "%;background:rgba(255," + (r_bg - 25) + "," + r_bg + ",1);-webkit-animation:love " + r_time + "s ease;-moz-animation:love " + r_time + "s ease;-ms-animation:love " + r_time + "s ease;animation:love " + r_time + "s ease'></div>");
+
+            // $('.bg_heart').append("<div class='heart' style='width:" + (r_size - 10) + "px;height:" + (r_size - 10) + "px;left:" + (r_left + r_num) + "%;background:rgba(255," + (r_bg - 25) + "," + (r_bg + 25) + ",1);-webkit-animation:love " + (r_time + 5) + "s ease;-moz-animation:love " + (r_time + 5) + "s ease;-ms-animation:love " + (r_time + 5) + "s ease;animation:love " + (r_time + 5) + "s ease'></div>");
+
+            // $('.heart').each(function() {
+            //     var top = $(this).css("top").replace(/[^-\d\.]/g, '');
+            //     var width = $(this).css("width").replace(/[^-\d\.]/g, '');
+            //     if (top <= -100 || width >= 150) {
+            //         $(this).detach();
+            //     }
+            // });
+
+        }, 500)
+
+    })
+    */
 
 </script>
 
 
 <template>
 
-    <div class="text-center px-6 pt-20">
+    <div class="relative w-full h-full top-0 left-0 overflow-hidden" ref="heartsWorkspaceDiv">
 
         <div class="max-w-2xl mx-auto text-center text-3xl">
             Happy Mother's Day!
         </div>
-        
-        <div class="max-w-2xl grid grid-cols-5 items-center gap-8 px-8 pb-10 mx-auto">
 
-            <img alt="Vue logo" src="../assets/logo-vue.svg"/>
-            <img alt="Tailwind CSS logo" src="../assets/logo-tailwindcss.svg"/>
-            <div>
-                <!-- div wrapper is needed so Safari contains the flex size of an svg/img child correctly -->
-                <VueVentusSpinningMark animation-class="animate-spin-reverse-4s"/>
-            </div>
-            <img alt="Vite.js logo" src="../assets/logo-vite.svg"/>
-            <img alt="Typescript logo" src="../assets/logo-typescript.svg"/>
-
-        </div>
-
-        <div class="w-full flex flex-col text-center space-y-3">
-
-            <h1 class="text-2xl">{{ msg }}</h1>
-
-            <p>
-                Recommended IDE setup: <VvAnchor href="https://code.visualstudio.com/" color="success" target="_blank">VS Code</VvAnchor> + <VvAnchor href="https://github.com/johnsoncodehk/volar" color="success" target="_blank">Volar</VvAnchor>
-            </p>
-
-            <p>
-                See <VvEl tag="span" ground-palette="monochromatic" ground-color="success" class="px-2 rounded">README.md</VvEl> for more information.
-            </p>
-
-            <p>
-                <VvAnchor href="https://vitejs.dev/guide/features.html" color="success" target="_blank">Vite Docs</VvAnchor> | <VvAnchor href="https://v3.vuejs.org/" color="success" target="_blank">Vue 3 Docs</VvAnchor>
-            </p>
-
-            <div class="py-4">
-                <VvButton color="primary" @click="count++">Count is: {{ count }}</VvButton>
-            </div>
-
-            <p>
-                Edit <VvEl tag="span" ground-palette="monochromatic" ground-color="success" class="px-2 rounded">components/HelloWorld.vue</VvEl> to test hot module replacement.
-            </p>
-
-            <div class="flex flex-col w-full max-w-xl gap-10 mx-auto text-center pt-8">
-            
-                <div>
-
-                    <VvAnchor href="https://github.com/obewds/vueventus" color="primary">
-                        VvAnchor
-                    </VvAnchor>
-                    <span> | </span>
-                    <VvRouterLink to="/" color="primary">
-                        VvRouterLink Test
-                    </VvRouterLink>
-
-                </div>
-            
-                <div>
-
-                    <VvButton color="primary">
-                        VvButton Test
-                    </VvButton>
-
-                </div>
-            
-                <div>
-
-                    <VvEl color="primary" palette="solid">
-                        VvEl Test Div
-                    </VvEl>
-
-                </div>
-            
-                <div>
-
-                    <VvFa family="fas" icon="house" size="3x"/>
-
-                </div>
-
-                <div class="text-left">
-
-                    <VvFormGroup
-                        label="VvFormGroup Test:"
-                        label-for="vv-input-test"
-                        :display-help="true"
-                        help-text="This is example help text"
-                    >
-                        <VvInput id="vv-input-test" color="default" placeholder="VvInput Test"/>
-                    </VvFormGroup>
-
-                </div>
-
-                <div class="text-left">
-
-                    <VvQuadFormGroup
-                        label="VvQuadFormGroup Test:"
-                        label-for="vv-textarea-test"
-                        :display-help="true"
-                        help-text="This is example help text"
-                        top-wrapper-classes="flex justify-between items-end gap-3"
-                    >
-                        <template v-slot:top>
-                            <button class="text-xl">&times;</button>
-                        </template>
-
-                        <VvTextarea id="vv-textarea-test" color="default" placeholder="VvTextarea Test"/>
-
-                        <template v-slot:bottom>
-                            <VvEl size="sm">0/100</VvEl>
-                        </template>
-                    </VvQuadFormGroup>
-
-                </div>
-
-                <div class="text-left">
-
-                    <div class="flex flex-col w-full space-y-2">
-
-                        <div class="flex items-center gap-2">
-                            <VvCheckbox id="option-one" color="primary" :checked="true"/>
-                            <label for="option-one">The First Option</label>
-                        </div>
-
-                        <div class="flex items-center gap-2">
-                            <VvCheckbox id="option-two" color="primary"/>
-                            <label for="option-two">The Second Option</label>
-                        </div>
-
-                    </div>
-
-                </div>
-
-                <div class="text-left">
-
-                    <div class="flex flex-col space-y-1">
-                        <div class="flex items-center gap-2">
-                            <VvRadio
-                                id="lightType-1"
-                                name="lightType"
-                                value="sunlight"
-                                :checked="true"
-                                color="primary"
-                            />
-                            <label for="lightType-1">Sunlight</label>
-                        </div>
-                        <div class="flex items-center gap-2">
-                            <VvRadio
-                                id="lightType-2"
-                                name="lightType"
-                                value="moonlight"
-                                :checked="false"
-                                color="primary"
-                            />
-                            <label for="lightType-2">Moonlight</label>
-                        </div>
-                    </div>
-
-                </div>
-
-                <div class="text-left">
-
-                    <VvList list-style-type-class="list-disc" class="pl-3">
-                        <li>.list-disc Item One</li>
-                        <li>.list-disc Item Two</li>
-                    </VvList>
-
-                </div>
-
-                <div>
-
-                    <ul class="w-full list-disc text-left pl-4">
-
-                        <VvListItem>VvListItem Test</VvListItem>
-
-                        <VvListItem symbol-color="success">VvListItem Test</VvListItem>
-
-                    </ul>
-
-                </div>
-
-                <div>
-                    <VvListbox
-                        label="VvListbox Example:"
-                        listbox-options-without-label-spacing="pt-1"
-                        listbox-options-with-label-spacing="pt-1"
-                    />
-                </div>
-
-                <div>
-                    <VvListbox
-                        listbox-options-without-label-spacing="pt-1"
-                        listbox-options-with-label-spacing="pt-1"
-                    />
-                </div>
-
-                <div>
-                    <VvSelect>
-                        <option value="">Select an Option</option>
-                        <option value="one">Default Color Option 1</option>
-                        <option value="two">Default Color Option 2</option>
-                    </VvSelect>
-                </div>
-
-            </div>
-
-            <div class="max-w-4xl pt-12 text-left mx-auto">
-            
-                <p class="text-lg font-bold pb-1">Current <VvEl tag="span" ground-palette="monochromatic" ground-color="success" class="px-2 rounded">./src/app.vv.ts</VvEl> Configuration Values:</p>
-            
-                <VvPrism>
-                    <pre>
-                        <code class="line-numbers language-javascript">
-                            {{ appVv }}
-                        </code>
-                    </pre>
-                </VvPrism>
-            
-            </div>
-
-        </div>
+        <ul class="text-lg py-6">
+            <!--<li>
+                Do mother's make the 
+                <button @click="swap1 = !swap1">
+                    <span v-if="!swap1">
+                        world
+                    </span>
+                    <VvFa v-else family="fas" icon="house"/>
+                </button> 
+                go 'round?
+            </li>-->
+            <li>Do mother's make</li>
+            <li>The world go around?</li>
+            <li>When one picks you up,</li>
+            <li>After you've fallen down?</li>
+            <li>Or maybe when one</li>
+            <li>Sees a terrible day,</li>
+            <li>And then makes some food</li>
+            <li>That can wash it away?</li>
+            <li>And what about sick times,</li>
+            <li>With snot gobs and goop?</li>
+            <li>It's mother's that always</li>
+            <li>Know just what to do!</li>
+            <li>From bedtime stories</li>
+            <li>To moments of truth,</li>
+            <li>Mom's persevere,</li>
+            <li>And they do it for you.</li>
+            <li>So once every year,</li>
+            <li>We all take a day,</li>
+            <li>To tell our mom's thank you</li>
+            <li>In meaningful ways.</li>
+            <li>Because mom's are precious.</li>
+            <li>And special. And rare.</li>
+            <li>They deserve our respect,</li>
+            <li>And our love and our care.</li>
+            <li>So thank you dear mamas,</li>
+            <li>For all that you do,</li>
+            <li>You brought us all here,</li>
+            <li>And you teach us stuff, too!</li>
+            <li>So no matter what happens,</li>
+            <li>Remember this truth:</li>
+            <li>There is no hero like</li>
+            <li>The mom inside of you.</li>
+        </ul>
 
     </div>
 
 </template>
 
+<style>
+
+.heart {
+    position: absolute;
+    top: -50%;
+    -webkit-transform: rotate(-45deg);
+    -moz-transform: rotate(-45deg);
+    -m-transform: rotate(-45deg);
+    transform: rotate(-45deg)
+ }
+
+.heart:before {
+    position: absolute;
+    top: -50%;
+    left: 0;
+    display: block;
+    content: "";
+    width: 100%;
+    height: 100%;
+    background: inherit;
+    border-radius: 100%;
+}
+
+.heart:after {
+    position: absolute;
+    top: 0;
+    right: -50%;
+    display: block;
+    content: "";
+    width: 100%;
+    height: 100%;
+    background: inherit;
+    border-radius: 100%;
+}
+
+@-webkit-keyframes love {
+  0%{top:110%}
+}
+@-moz-keyframes love {
+  0%{top:110%}
+}
+@-ms-keyframes love {
+  0%{top:110%}
+}
+@keyframes love {
+  0%{top:110%}
+}
+
+</style>
